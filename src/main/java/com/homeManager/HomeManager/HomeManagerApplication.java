6f458ba0e96133186677c6b3f8856183674dae7a
vishalmishra4u <mishravishal0912@gmail.com>
2019-04-14 23:54:25 +0530

    first commit

---
+++ src/main/java/com/homeManager/HomeManager/HomeManagerApplication.java
@@ -0,0 +1,13 @@
+package com.homeManager.HomeManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeManagerApplication.class, args);
+	}
+
+}
